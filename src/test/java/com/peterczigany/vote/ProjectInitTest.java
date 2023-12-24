ee171af3f85e11546c11cf69391ae5fbf25ac6f4
Peter Czigany <p.czigany@gmail.com>
2023-12-24 12:08:19 +0100

    add empty test method

---
+++ src/test/java/com/peterczigany/vote/ProjectInitTest.java
@@ -0,0 +1,10 @@
+package com.peterczigany.vote;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectInitTest {
+
+  @SuppressWarnings("java:S2699") // this is an empty test method by definition
+  @Test
+  void emptyTest() {}
+}
