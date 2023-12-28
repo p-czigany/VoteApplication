4eee9b70cf7a8130031b0d54df7f4a2923ae4609
Peter Czigany <p.czigany@gmail.com>
2023-12-28 17:37:40 +0100

    add method test

--- src/test/java/com/peterczigany/vote/ProjectInitTest.java
+++ src/test/java/com/peterczigany/vote/ProjectInitTest.java
@@ -1,10 +1,17 @@
 package com.peterczigany.vote;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class ProjectInitTest {
+
+  static class MyObject {
+    String getOk() {
+      return "ok";
+    }
+  }
 
   @SuppressWarnings("java:S2699") // this is an empty test method by definition
   @Test
@@ -13,5 +20,11 @@ class ProjectInitTest {
   @Test
   void testStringEquality() {
     assertEquals("ok", "ok");
+  }
+
+  @Test
+  void testStringReturnValue() {
+    MyObject myObject = new MyObject();
+    assertThat(myObject.getOk()).isExactlyInstanceOf(String.class);
   }
 }
