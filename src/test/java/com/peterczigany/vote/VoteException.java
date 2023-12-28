c7af9d12e318ece8d8cb311d9bffd1f783502363
Peter Czigany <p.czigany@gmail.com>
2023-12-28 18:49:53 +0100

    refactor

--- src/test/java/com/peterczigany/vote/VoteException.java
+++ src/test/java/com/peterczigany/vote/VoteException.java
@@ -1,7 +1,7 @@
 package com.peterczigany.vote;
 
 public class VoteException extends Exception {
-  public VoteException(String s) {
-    super(s);
+  public VoteException(String message) {
+    super(message);
   }
 }
