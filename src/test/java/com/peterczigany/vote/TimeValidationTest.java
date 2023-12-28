14d61ebd513fa74bcb7866202838a8ca57ee9e91
Peter Czigany <p.czigany@gmail.com>
2023-12-28 19:21:39 +0100

    false positive :/

--- src/test/java/com/peterczigany/vote/TimeValidationTest.java
+++ src/test/java/com/peterczigany/vote/TimeValidationTest.java
@@ -3,14 +3,19 @@ package com.peterczigany.vote;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import org.junit.jupiter.api.Test;
 
 class TimeValidationTest {
 
   private static final String TIME_NULL =
-      "Az időpont nem lehet null. Megadása kötelező. (ÉÉÉÉ-HH-NNTÓÓ:PP:MMZ formátumban.)";
+      "Az időpont nem lehet null. Megadása kötelező.";
   private static final String TIME_EMPTY =
-      "Az időpont nem lehet üres. Megadása kötelező. (ÉÉÉÉ-HH-NNTÓÓ:PP:MMZ formátumban.)";
+      "Az időpont nem lehet üres. Megadása kötelező.";
+  private static final String TIME_BAD_FORMAT =
+      "Az időpont formátuma nem megfelelő. ISO8601 (YYYY-MM-DDThh:mm:ssZ) formátumban szükséges megadni.)";
 
   static class MyValidator {
     void validateTime(String timeString) throws VoteException {
@@ -19,6 +24,11 @@ class TimeValidationTest {
       }
       if (timeString.isEmpty()) {
         throw new VoteException(TIME_EMPTY);
+      }
+      try {
+        LocalDateTime.parse(timeString, DateTimeFormatter.ISO_INSTANT);
+      } catch (DateTimeParseException e) {
+        throw new VoteException(TIME_BAD_FORMAT);
       }
     }
   }
@@ -39,5 +49,14 @@ class TimeValidationTest {
     Throwable exception = assertThrows(VoteException.class, () -> myValidator.validateTime(""));
 
     assertThat(exception.getMessage()).isEqualTo(TIME_EMPTY);
+  }
+
+  @Test
+  void testTimeMustBeISO8601String() throws VoteException {
+    MyValidator myValidator = new MyValidator();
+
+    Exception exception = assertThrows(VoteException.class, () -> myValidator.validateTime("2023-12-23T14:30:45Z"));
+
+    assertThat(exception.getMessage()).isEqualTo(TIME_BAD_FORMAT);
   }
 }
