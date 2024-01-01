881a0da924ccc16e112119ab76b9caf8e707e27c
Peter Czigany <p.czigany@gmail.com>
2024-01-01 19:12:09 +0100

    add test case for time to the second in ISO 8601 format

--- src/test/java/com/peterczigany/vote/TimeValidationTest.java
+++ src/test/java/com/peterczigany/vote/TimeValidationTest.java
@@ -1,19 +1,17 @@
 package com.peterczigany.vote;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import org.junit.jupiter.api.Test;
 
 class TimeValidationTest {
 
-  private static final String TIME_NULL =
-      "Az időpont nem lehet null. Megadása kötelező.";
-  private static final String TIME_EMPTY =
-      "Az időpont nem lehet üres. Megadása kötelező.";
+  private static final String TIME_NULL = "Az időpont nem lehet null. Megadása kötelező.";
+  private static final String TIME_EMPTY = "Az időpont nem lehet üres. Megadása kötelező.";
   private static final String TIME_BAD_FORMAT =
       "Az időpont formátuma nem megfelelő. ISO8601 (YYYY-MM-DDThh:mm:ssZ) formátumban szükséges megadni.)";
 
@@ -26,7 +24,7 @@ class TimeValidationTest {
         throw new VoteException(TIME_EMPTY);
       }
       try {
-        LocalDateTime.parse(timeString, DateTimeFormatter.ISO_INSTANT);
+        LocalDateTime.parse(timeString);
       } catch (DateTimeParseException e) {
         throw new VoteException(TIME_BAD_FORMAT);
       }
@@ -55,8 +53,19 @@ class TimeValidationTest {
   void testTimeMustBeISO8601String() throws VoteException {
     MyValidator myValidator = new MyValidator();
 
-    Exception exception = assertThrows(VoteException.class, () -> myValidator.validateTime("2023-12-23T14:30:45Z"));
+    Exception exception =
+        assertThrows(VoteException.class, () -> myValidator.validateTime("2023-12-23 14:30:45"));
 
     assertThat(exception.getMessage()).isEqualTo(TIME_BAD_FORMAT);
+  }
+
+  @Test
+  void testSuccessfulTimeStringValidation() {
+    MyValidator myValidator = new MyValidator();
+
+    assertDoesNotThrow(() -> myValidator.validateTime("2023-12-23T14:30:45Z"));
+    assertDoesNotThrow(() -> myValidator.validateTime("2023-12-23T14:30:45+01:00"));
+    assertDoesNotThrow(() -> myValidator.validateTime("2023-12-23T14:30:45+0100"));
+    assertDoesNotThrow(() -> myValidator.validateTime("2023-12-23T14:30:45+01"));
   }
 }
