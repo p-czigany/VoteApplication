b4a7cf392f943f85a679d10fe7bf3b7b786b6598
Peter Czigany <p.czigany@gmail.com>
2024-01-02 13:59:00 +0100

    add voting session validator tests

---
+++ src/test/java/com/peterczigany/vote/validator/VotingSessionValidatorTest.java
@@ -0,0 +1,46 @@
+package com.peterczigany.vote.validator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.peterczigany.vote.VoteException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class VotingSessionValidatorTest {
+
+  private static VotingSessionValidator votingSessionValidator;
+
+  @BeforeAll
+  static void setup() {
+    votingSessionValidator =
+        new VotingSessionValidator(
+            new TimeValidator(),
+            new SubjectValidator(),
+            new TypeValidator(),
+            new RepresentativeCodeValidator(),
+            new VoteValidator(new RepresentativeCodeValidator(), new VoteValueValidator()));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/validVotingSessions.csv")
+  void testValidVotingSession(String votingSessionJson) {
+    assertDoesNotThrow(() -> votingSessionValidator.validateVotingSession(votingSessionJson));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(
+      resources = {
+        "/invalidVotingSessions/badTimeFormat.csv",
+        "/invalidVotingSessions/blankField.csv",
+        "/invalidVotingSessions/chairDidNotVote.csv",
+        "/invalidVotingSessions/invalidType.csv",
+        "/invalidVotingSessions/invalidVoteValue.csv",
+        "/invalidVotingSessions/missingField.csv"
+      })
+  void testInvalidVotingSession(String votingSessionJson) {
+    assertThrows(
+        VoteException.class, () -> votingSessionValidator.validateVotingSession(votingSessionJson));
+  }
+}
