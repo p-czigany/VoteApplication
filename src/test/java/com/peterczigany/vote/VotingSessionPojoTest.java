930fe697a1911e27bff1127aec64d040710224ed
Peter Czigany <p.czigany@gmail.com>
2024-01-04 11:24:52 +0100

    add voting session pojo constructor test

--- src/test/java/com/peterczigany/vote/VotingSessionPojoTest.java
+++ src/test/java/com/peterczigany/vote/VotingSessionPojoTest.java
@@ -1,6 +1,7 @@
 package com.peterczigany.vote;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.peterczigany.vote.VotingSession.Vote;
 import java.time.ZoneId;
@@ -9,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class VotingSessionPojoTest {
+
   @Test
   void testVotingSessionCreationIsSuccessful() {
     Vote vote1 = new Vote("Kepviselo1", "i");
@@ -25,5 +27,17 @@ class VotingSessionPojoTest {
     assertThat(votingSession.votes().get(0)).isEqualTo(vote1);
     assertThat(votingSession.votes().get(1).representative()).isEqualTo("Kepviselo2");
     assertThat(votingSession.votes().get(1).voteValue()).isEqualTo("n");
+  }
+
+  @Test
+  void testVotingSessionCreationIsUnsuccessful() {
+    Vote vote1 = new Vote("Kepviselo1", "i");
+    List<Vote> votes = List.of(vote1, new Vote("Kepviselo2", "n"), new Vote("Kepviselo3", "t"));
+
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z", "Subject of Voting", "j", "Kepviselo1", votes))
+        .isInstanceOf(Exception.class);
   }
 }
