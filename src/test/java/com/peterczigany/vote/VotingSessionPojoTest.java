9a81dfa98d5b307df88b4ec506a8cf6ff1f66723
Peter Czigany <p.czigany@gmail.com>
2024-01-05 18:04:45 +0100

    validate voting session pojo

--- src/test/java/com/peterczigany/vote/VotingSessionPojoTest.java
+++ src/test/java/com/peterczigany/vote/VotingSessionPojoTest.java
@@ -3,11 +3,16 @@ package com.peterczigany.vote;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.peterczigany.vote.VotingSession.Type;
 import com.peterczigany.vote.VotingSession.Vote;
+import com.peterczigany.vote.VotingSession.Vote.VoteValue;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class VotingSessionPojoTest {
 
@@ -22,19 +27,31 @@ class VotingSessionPojoTest {
     assertThat(votingSession.time())
         .isEqualTo(ZonedDateTime.of(2023, 12, 23, 14, 30, 45, 0, ZoneId.of("UTC")));
     assertThat(votingSession.subject()).isEqualTo("Subject of Voting");
-    assertThat(votingSession.type()).isEqualTo("j");
+    assertThat(votingSession.type()).isEqualTo(Type.PRESENCE);
     assertThat(votingSession.chair()).isEqualTo("Kepviselo1");
     assertThat(votingSession.votes().get(0)).isEqualTo(vote1);
     assertThat(votingSession.votes().get(1).representative()).isEqualTo("Kepviselo2");
-    assertThat(votingSession.votes().get(1).voteValue()).isEqualTo("n");
+    assertThat(votingSession.votes().get(1).voteValue()).isEqualTo(VoteValue.AGAINST);
   }
 
-  @Test
-  void testVotingSessionCreationWithInvalidTimeString() {
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(
+      strings = {
+        "",
+        " ",
+        "2023-12-23 14:30:45",
+        "2023-12-23T14:30:45+0130",
+        "20240101T173952Z",
+        "2024-01-01T10:39:52−08:00",
+        "2024-01-01T10:39:52−08",
+        "2024-01-01T10:39:52−0630"
+      })
+  void testVotingSessionCreationWithMissingOrInvalidTimeString(String timeString) {
     assertThatThrownBy(
             () ->
                 new VotingSession(
-                    "2023-12-23 14:30:45",
+                    timeString,
                     "Subject of Voting",
                     "j",
                     "Kepviselo1",
@@ -45,83 +62,91 @@ class VotingSessionPojoTest {
         .isInstanceOf(Exception.class);
   }
 
-//  @Test
-//  void testVotingSessionCreationWithoutChairVote() {
-//    assertThatThrownBy(
-//            () ->
-//                new VotingSession(
-//                    "2023-12-23 14:30:45Z",
-//                    "Subject of Voting",
-//                    "j",
-//                    "Kepviselo0",
-//                    List.of(
-//                        new Vote("Kepviselo1", "i"),
-//                        new Vote("Kepviselo2", "n"),
-//                        new Vote("Kepviselo3", "t"))))
-//        .isInstanceOf(Exception.class);
-//  }
-//
-//  @Test
-//  void testVotingSessionCreationWithInvalidType() {
-//    assertThatThrownBy(
-//            () ->
-//                new VotingSession(
-//                    "2023-12-23 14:30:45Z",
-//                    "Subject of Voting",
-//                    "jelenléti (NOT VALID)",
-//                    "Kepviselo1",
-//                    List.of(
-//                        new Vote("Kepviselo1", "i"),
-//                        new Vote("Kepviselo2", "n"),
-//                        new Vote("Kepviselo3", "t"))))
-//        .isInstanceOf(Exception.class);
-//  }
-//
-//  @Test
-//  void testVotingSessionCreationWithInvalidVoteValue() {
-//    assertThatThrownBy(
-//            () ->
-//                new VotingSession(
-//                    "2023-12-23 14:30:45Z",
-//                    "Subject of Voting",
-//                    "j",
-//                    "Kepviselo1",
-//                    List.of(
-//                        new Vote("Kepviselo1", "igen (NOT VALID)"),
-//                        new Vote("Kepviselo2", "n"),
-//                        new Vote("Kepviselo3", "t"))))
-//        .isInstanceOf(Exception.class);
-//  }
-//
-//  @Test
-//  void testVotingSessionCreationWithEmptyField() {
-//    assertThatThrownBy(
-//            () ->
-//                new VotingSession(
-//                    "2023-12-23 14:30:45Z",
-//                    "",
-//                    "j",
-//                    "Kepviselo1",
-//                    List.of(
-//                        new Vote("Kepviselo1", "i"),
-//                        new Vote("Kepviselo2", "n"),
-//                        new Vote("Kepviselo3", "t"))))
-//        .isInstanceOf(Exception.class);
-//  }
-//
-//  @Test
-//  void testVotingSessionCreationWithNullField() {
-//    assertThatThrownBy(
-//            () ->
-//                new VotingSession(
-//                    "2023-12-23 14:30:45Z",
-//                    null,
-//                    "j",
-//                    "Kepviselo1",
-//                    List.of(
-//                        new Vote("Kepviselo1", "i"),
-//                        new Vote("Kepviselo2", "n"),
-//                        new Vote("Kepviselo3", "t"))))
-//        .isInstanceOf(Exception.class);
-//  }
+  @Test
+  void testVotingSessionCreationWithoutChairVote() {
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z",
+                    "Subject of Voting",
+                    "j",
+                    "Kepviselo0",
+                    List.of(
+                        new Vote("Kepviselo1", "i"),
+                        new Vote("Kepviselo2", "n"),
+                        new Vote("Kepviselo3", "t"))))
+        .isInstanceOf(Exception.class);
+  }
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(strings = {"", " ", "jelenléti (NOT VALID)"})
+  void testVotingSessionCreationWithMissingOrInvalidType(String type) {
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z",
+                    "Subject of Voting",
+                    type,
+                    "Kepviselo1",
+                    List.of(
+                        new Vote("Kepviselo1", "i"),
+                        new Vote("Kepviselo2", "n"),
+                        new Vote("Kepviselo3", "t"))))
+        .isInstanceOf(Exception.class);
+  }
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(strings = {"", " ", "igen (NOT VALID)"})
+  void testVotingSessionCreationWithMissingOrInvalidVoteValue(String voteValue) {
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z",
+                    "Subject of Voting",
+                    "j",
+                    "Kepviselo1",
+                    List.of(
+                        new Vote("Kepviselo1", voteValue),
+                        new Vote("Kepviselo2", "n"),
+                        new Vote("Kepviselo3", "t"))))
+        .isInstanceOf(Exception.class);
+  }
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(strings = {"", " "})
+  void testVotingSessionCreationWithMissingOrEmptySubject(String subject) {
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z",
+                    subject,
+                    "j",
+                    "Kepviselo1",
+                    List.of(
+                        new Vote("Kepviselo1", "i"),
+                        new Vote("Kepviselo2", "n"),
+                        new Vote("Kepviselo3", "t"))))
+        .isInstanceOf(Exception.class);
+  }
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(strings = {"", " "})
+  void testVotingSessionCreationWithMissingOrEmptyRepresentative(String representative) {
+    assertThatThrownBy(
+            () ->
+                new VotingSession(
+                    "2023-12-23 14:30:45Z",
+                    "Subject of Voting",
+                    "j",
+                    "Kepviselo1",
+                    List.of(
+                        new Vote("Kepviselo1", "i"),
+                        new Vote(representative, "n"),
+                        new Vote("Kepviselo3", "t"))))
+        .isInstanceOf(Exception.class);
+  }
 }
